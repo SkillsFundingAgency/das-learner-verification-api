30a9bd5344ba3910a40117469c424b315ff23221
Tom Gough <tom.gough@talentconsulting.uk>
2023-03-03 11:52:34 +0000

    FLP-250 Added logging into error scenario in CertificateProvider

--- src/SFA.DAS.LearnerVerification.Domain/Services/CertificateProvider.cs
+++ src/SFA.DAS.LearnerVerification.Domain/Services/CertificateProvider.cs
@@ -1,17 +1,20 @@
 ﻿using Azure.Identity;
 using Azure.Security.KeyVault.Certificates;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Logging;
 
 namespace SFA.DAS.LearnerVerification.Domain.Factories
 {
     public class CertificateProvider : ICertificateProvider
     {
         private readonly LrsApiWcfSettings _lrsApiSettings;
+        private readonly ILogger<CertificateProvider> _logger;
         private X509Certificate2 _x509Certificate;
 
-        public CertificateProvider(LrsApiWcfSettings lrsApiSettings)
+        public CertificateProvider(LrsApiWcfSettings lrsApiSettings, ILogger<CertificateProvider> logger)
         {
             _lrsApiSettings = lrsApiSettings;
+            _logger = logger;
             SetupClientCertificate();
         }
 
@@ -48,7 +51,7 @@ namespace SFA.DAS.LearnerVerification.Domain.Factories
             }
             catch (Exception ex)
             {
-                //TODO: Logging?
+                _logger.LogError(ex, "Error setting up client certificate");
                 throw;
             }
         }
