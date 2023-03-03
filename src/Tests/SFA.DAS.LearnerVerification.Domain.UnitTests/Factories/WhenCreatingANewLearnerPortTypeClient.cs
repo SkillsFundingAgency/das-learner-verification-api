413ad6fed1baa7284cfd3e6aa84965b68186c32f
Tom Gough <tom.gough@talentconsulting.uk>
2023-03-03 13:52:27 +0000

    FLP-250 Added initial unit tests for LearnerPortTypeClientFactory

--- src/Tests/SFA.DAS.LearnerVerification.Domain.UnitTests/Factories/WhenCreatingANewLearnerPortTypeClient.cs
+++ src/Tests/SFA.DAS.LearnerVerification.Domain.UnitTests/Factories/WhenCreatingANewLearnerPortTypeClient.cs
@@ -2,35 +2,47 @@ using AutoFixture;
 using Moq;
 using SFA.DAS.LearnerVerification.Domain.Factories;
 using System.ServiceModel;
+using FluentAssertions;
 using SFA.DAS.LearnerVerification.Domain.Services;
+using LearningRecordsService;
 
 namespace SFA.DAS.LearnerVerification.Domain.UnitTests.Factories
 {
     public class WhenCreatingANewLearnerPortTypeClient
     {
         private Fixture _fixture;
-        private Mock<ICertificateProvider> _mockCertificateProvider;
-        private Mock<LrsApiWcfSettings> _mockLrsApiWcfSettings;
+        private LrsApiWcfSettings _settings;
         private LearnerPortTypeClientFactory _sut;
+
+        private LearnerPortTypeClient _client;
 
         [SetUp]
         public void Setup()
         {
+            //Arrange
             _fixture = new Fixture();
-            _sut = new LearnerPortTypeClientFactory(_mockLrsApiWcfSettings.Object, _mockCertificateProvider.Object);
+            _settings = new LrsApiWcfSettings
+            {
+                LearnerServiceBaseUrl = "http://valid.test.url/"
+            };
+            _sut = new LearnerPortTypeClientFactory(_settings, Mock.Of<ICertificateProvider>());
+
+            var binding = _fixture.Create<BasicHttpBinding>();
+
+            //Act
+            _client = _sut.Create(binding);
         }
 
         [Test]
         public void ThenALearnerPortTypeClientIsCreated()
         {
-            //Arrange
-            var binding = _fixture.Create<BasicHttpBinding>();
+            _client.Should().NotBeNull();
+        }
 
-            //Act
-            var client = _sut.Create(binding);
-
-            //Assert
-            //TODO:
+        [Test]
+        public void ThenALearnerPortTypeClientHasTheCorrectUrl()
+        {
+            _client.Endpoint.Address.ToString().Should().Be(_settings.LearnerServiceBaseUrl);
         }
     }
 }
