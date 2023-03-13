57b0140707486131f77f85b52b26a061769b88d0
dinathain <55487881+dinathain@users.noreply.github.com>
2023-03-13 11:47:39 +0000

    Added validation for service base url

--- src/Tests/SFA.DAS.LearnerVerification.Domain.UnitTests/Factories/WhenCreatingANewLearnerPortTypeClient.cs
+++ src/Tests/SFA.DAS.LearnerVerification.Domain.UnitTests/Factories/WhenCreatingANewLearnerPortTypeClient.cs
@@ -5,6 +5,8 @@ using System.ServiceModel;
 using FluentAssertions;
 using SFA.DAS.LearnerVerification.Domain.Services;
 using LearningRecordsService;
+using System.ServiceModel.Channels;
+using System.Security.Policy;
 
 namespace SFA.DAS.LearnerVerification.Domain.UnitTests.Factories
 {
@@ -13,6 +15,7 @@ namespace SFA.DAS.LearnerVerification.Domain.UnitTests.Factories
         private LearnerPortTypeClient _client;
         private Fixture _fixture;
         private LrsApiWcfSettings _settings;
+        private BasicHttpBinding _basicHttpBinding;
         private LearnerPortTypeClientFactory _sut;
 
         [SetUp]
@@ -20,27 +23,66 @@ namespace SFA.DAS.LearnerVerification.Domain.UnitTests.Factories
         {
             //Arrange
             _fixture = new Fixture();
+            _basicHttpBinding = _fixture.Create<BasicHttpBinding>();
+        }
+
+        private void AddLearnerServiceBaseUrl(string? url)
+        {
+            _settings = new LrsApiWcfSettings
+            {
+                LearnerServiceBaseUrl = url
+            };
+        }
+
+        private void AddValidLearnerServiceBaseUrl()
+        {
             _settings = new LrsApiWcfSettings
             {
                 LearnerServiceBaseUrl = "http://valid.test.url/"
             };
-            _sut = new LearnerPortTypeClientFactory(_settings, Mock.Of<ICertificateProvider>());
+        }
 
-            var binding = _fixture.Create<BasicHttpBinding>();
+        [TestCase(null)]
+        [TestCase("")]
+        public void AndKeyVaultConfigIsNotSetThenThrowException(string url)
+        {
+            //Arrange
+            AddLearnerServiceBaseUrl(url);
 
             //Act
-            _client = _sut.Create(binding);
+            Action act = () => { _ = new LearnerPortTypeClientFactory(_settings, Mock.Of<ICertificateProvider>()); };
+
+            //Assert
+            act.Should()
+                .Throw<ArgumentNullException>()
+                .WithMessage("LearnerServiceBaseUrl is not specified. This is required to run the app. (Parameter 'LearnerServiceBaseUrl')");
         }
 
         [Test]
         public void ThenALearnerPortTypeClientHasTheCorrectUrl()
         {
+            //Arrange
+            AddValidLearnerServiceBaseUrl();
+            _sut = new LearnerPortTypeClientFactory(_settings, Mock.Of<ICertificateProvider>());
+
+            //Act
+            _client = _sut.Create(_basicHttpBinding);
+
+            //Assert
             _client.Endpoint.Address.ToString().Should().Be(_settings.LearnerServiceBaseUrl);
         }
 
         [Test]
         public void ThenALearnerPortTypeClientIsCreated()
         {
+            //Arrange
+            AddValidLearnerServiceBaseUrl();
+            _sut = new LearnerPortTypeClientFactory(_settings, Mock.Of<ICertificateProvider>());
+
+            //Act
+            _client = _sut.Create(_basicHttpBinding);
+
+            //Assert
             _client.Should().NotBeNull();
             _client.Should().BeOfType<LearnerPortTypeClient>();
         }
