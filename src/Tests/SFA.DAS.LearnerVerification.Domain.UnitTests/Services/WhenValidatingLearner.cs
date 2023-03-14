402f582f7eafb58ab5256bb19d499900d223a24c
Tom Gough <tom.gough@talentconsulting.uk>
2023-03-14 08:35:07 +0000

    FLP-250 Added test coverage for LearnerValidationService

--- src/Tests/SFA.DAS.LearnerVerification.Domain.UnitTests/Services/WhenValidatingLearner.cs
+++ src/Tests/SFA.DAS.LearnerVerification.Domain.UnitTests/Services/WhenValidatingLearner.cs
@@ -14,6 +14,9 @@ namespace SFA.DAS.LearnerVerification.Domain.UnitTests.Services
         private Mock<ILearnerVerificationServiceClientProvider> _mockClientProvider;
         private LearnerValidationService _sut;
 
+        private readonly string _expectedFamilyName = "Dwyer";
+        private readonly string _expectedGivenName = "Andy";
+
         public WhenValidatingLearner()
         {
             _mockClientWrapper = new();
@@ -30,7 +33,11 @@ namespace SFA.DAS.LearnerVerification.Domain.UnitTests.Services
                 {
                     VerifyLearnerResponse = new VerifyLearnerResp()
                     {
-                        VerifiedLearner = new MIAPVerifiedLearner()
+                        VerifiedLearner = new MIAPVerifiedLearner
+                        {
+                            FamilyName = _expectedFamilyName,
+                            GivenName = _expectedGivenName
+                        }
                     }
                 });
 
@@ -49,8 +56,32 @@ namespace SFA.DAS.LearnerVerification.Domain.UnitTests.Services
 
             //Assert
             _learner.Should().BeOfType<MIAPVerifiedLearner>();
-            //TODO: Add more assertions
-            // verify verifyLearnerAsync() is called with the correct args
+            _learner.FamilyName.Should().Be(_expectedFamilyName);
+            _learner.GivenName.Should().Be(_expectedGivenName);
+        }
+
+        [Test]
+        public async Task ThenVerificationRequestIsBuiltCorrectly()
+        {
+            //Arrange
+            var uln = "912345678";
+            var firstName = "April";
+            var lastName = "Ludgate";
+            var gender = "F";
+            var dateOfBirth = DateTime.UtcNow.AddYears(-20);
+
+            //Act
+            _learner = await _sut.ValidateLearner(uln, firstName, lastName, gender, dateOfBirth);
+
+            //Assert
+            _mockClientWrapper.Verify(x => 
+                x.verifyLearnerAsync(It.Is<VerifyLearnerRqst>(y => 
+                    y.LearnerToVerify.ULN == uln
+                    && y.LearnerToVerify.GivenName == firstName
+                    && y.LearnerToVerify.FamilyName == lastName
+                    && y.LearnerToVerify.Gender == gender
+                    && y.LearnerToVerify.DateOfBirth == dateOfBirth.ToString("yyyy-MM-dd")
+            )));
         }
     }
 }
